537f3c5f5a00ebd132c9650f421e27c21785bd80
Shani Mithani <shanimithani@gmail.com>
2023-08-01 09:28:45 -0400

    Seed branch with populated dishes

---
+++ db/seeds/dishes.sql
@@ -0,0 +1,12 @@
+INSERT INTO dishes (name, description, dish_type, price, rating)
+VALUES
+  ('Spaghetti Bolognese', 'Classic Italian pasta dish with meat sauce.', 'Pasta', 12.99, 4.5),
+  ('Grilled Chicken Salad', 'Healthy salad with grilled chicken and fresh veggies.', 'Salad', 9.99, 4.2),
+  ('Cheeseburger', 'Juicy beef patty with melted cheese.', 'Burger', 8.49, 4.8),
+  ('Margarita Pizza', 'Traditional Italian pizza with tomato sauce and mozzarella cheese.', 'Pizza', 10.99, 4.7),
+  ('Sushi Platter', 'Assorted sushi rolls and sashimi.', 'Sushi', 18.99, 4.6),
+  ('Steak and Fries', 'Grilled steak served with crispy fries.', 'Steak', 22.99, 4.4),
+  ('Fish Tacos', 'Tacos filled with grilled fish and fresh toppings.', 'Tacos', 14.99, 4.3),
+  ('Chicken Alfredo', 'Creamy Alfredo sauce with grilled chicken and fettuccine.', 'Pasta', 13.99, 4.1),
+  ('Vegetable Stir-Fry', 'Assorted vegetables stir-fried with soy sauce.', 'Vegetarian', 11.99, 4.0),
+  ('Chocolate Brownie', 'Decadent chocolate brownie with ice cream.', 'Dessert', 6.99, 4.9);
