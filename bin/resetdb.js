1aa818e2ebe587f8fcc377c7b8b069176fe84f48
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-09-19 16:27:53 -0400

    test: reset:db should not run seed files

--- bin/resetdb.js
+++ bin/resetdb.js
@@ -23,24 +23,12 @@ const runSchemaFiles = async () => {
   }
 };
 
-const runSeedFiles = async () => {
-  console.log(chalk.cyan(`-> Loading Seeds ...`));
-  const schemaFilenames = fs.readdirSync('./db/seeds');
-
-  for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./db/seeds/${fn}`, 'utf8');
-    console.log(`\t-> Running ${chalk.green(fn)}`);
-    await db.query(sql);
-  }
-};
-
 const runResetDB = async () => {
   try {
     process.env.DB_HOST &&
       console.log(`-> Connecting to PG on ${process.env.DB_HOST} as ${process.env.DB_USER}...`);
 
     await runSchemaFiles();
-    await runSeedFiles();
     process.exit();
   } catch (err) {
     console.error(chalk.red(`Failed due to error: ${err}`));
