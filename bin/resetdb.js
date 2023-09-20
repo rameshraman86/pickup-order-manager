ced7f7935e41adfe0d8a10388fb9a27de454fb5d
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-09-20 14:56:24 -0400

    refactor: conditionally run seeds on db:reset

--- bin/resetdb.js
+++ bin/resetdb.js
@@ -6,18 +6,25 @@ const fs = require('fs');
 const chalk = require('chalk');
 const db = require('../db/connection');
 
-// PG connection setup
-// const connectionString = process.env.DATABASE_URL ||
-//   `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
-// const client = new Client();
-
 // Loads the schema files from db/schema
 const runSchemaFiles = async () => {
   console.log(chalk.cyan(`-> Loading Schema Files ...`));
   const schemaFilenames = fs.readdirSync('./db/schema');
 
+
   for (const fn of schemaFilenames) {
     const sql = fs.readFileSync(`./db/schema/${fn}`, 'utf8');
+    console.log(`\t-> Running ${chalk.green(fn)}`);
+    await db.query(sql);
+  }
+};
+
+const runSeedFiles = async () => {
+  console.log(chalk.cyan(`-> Loading Seed Files ...`));
+  const seedFilenames = fs.readdirSync('./db/seeds');
+
+  for (const fn of seedFilenames) {
+    const sql = fs.readFileSync(`./db/seeds/${fn}`, 'utf8');
     console.log(`\t-> Running ${chalk.green(fn)}`);
     await db.query(sql);
   }
@@ -29,6 +36,11 @@ const runResetDB = async () => {
       console.log(`-> Connecting to PG on ${process.env.DB_HOST} as ${process.env.DB_USER}...`);
 
     await runSchemaFiles();
+
+    const seedsFolderPath = './db/seeds';
+    if (fs.existsSync(seedsFolderPath)) {
+      await runSeedFiles();
+    }
     process.exit();
   } catch (err) {
     console.error(chalk.red(`Failed due to error: ${err}`));
